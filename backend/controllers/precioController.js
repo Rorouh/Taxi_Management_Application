d545a1d8feef47b6b5509d488f9ca25ef00dc79b
alejandroldz <alejandrold@correo.ugr.es>
2025-04-24 11:11:42 +0200

    simularPrecio

--- backend/controllers/precioController.js
+++ backend/controllers/precioController.js
@@ -26,6 +26,20 @@ exports.getPrecios = async (req, res) => {
 };
 
 exports.simularPrecio = async (req, res) => {
-  res.json({ mensaje: 'Simulación no implementada todavía' });
+  try {
+    const { horaInicio, horaFin, nivelConfort } = req.body;
+
+    const p = await Precio.findOne({ nivelConfort });
+    
+    if (horaInicio > horaFin) {
+      return res.status(400).json({ error: 'La hora de inicio debe ser anterior a la hora de fin' });
+    }
+
+    const duracion = (horaFin - horaInicio) / 60;
+    const precioTotal = (p.precioMinuto * duracion)+ (p.precioMinuto * duracion * p.incrementoNocturno / 100);
+    res.status(200).json(precioTotal);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
 };
 
